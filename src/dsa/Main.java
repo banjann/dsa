1dad28c58c1be2c8702fb02c31b25956e044713d
banjann <ivanjohnnaparota66@gmail.com>
2024-02-05 22:02:25 +0900

    added exercise

--- src/dsa/Main.java
+++ src/dsa/Main.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
 	public static void main(String[] args) {
-		HashMap<String, String> map = new HashMap<>();
-		map.put("easy", "easyOut");
-		map.put("easy", "easyOut2");
-
-		System.out.println(map.toString());
+		
+		
 	}
 
 	// 1 Contains Duplicate
@@ -76,5 +75,27 @@ public class Main {
 			}
 		}
 		return returnList;
+	}
+	
+	// 5 K Frequency
+	public int[] topKFrequent(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int num : nums) {
+			if (map.containsKey(num)) {
+				map.put(num, map.get(num) + 1);
+			} else {
+				map.put(num, 1);
+			}
+		}
+
+		List<Integer> listOfKeys = new ArrayList<>(map.keySet());
+		listOfKeys.sort((a, b) -> map.get(b) - map.get(a));
+
+		int[] result = new int[k];
+		for (int i = 0; i < k; i++) {
+			result[i] = listOfKeys.get(i);
+		}
+
+		return result;
 	}
 }
